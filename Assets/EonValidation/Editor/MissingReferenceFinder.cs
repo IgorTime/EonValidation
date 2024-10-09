14bbcb91bdf5f2c82e7fb6a9fdcb9f04fb29e815
Igor Semenov <igor.semenov.minsk@gmail.com>
2024-10-09 20:03:28 +0200

    Added some ignore types

--- Assets/EonValidation/Editor/MissingReferenceFinder.cs
+++ Assets/EonValidation/Editor/MissingReferenceFinder.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using EonValidation.Runtime;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace EonValidation.Editor
 {
     public static class MissingReferenceFinder
     {
+        private static readonly HashSet<Type> ignoredTypes = new()
+        {
+            typeof(GUISkin)
+        };
+        
         public static ValidationIssue[] FindMissingReferences(Object targetObject, Object context = null)
         {
+            if(targetObject == null)
+            {
+                return Array.Empty<ValidationIssue>();
+            }
+            
+            if (ignoredTypes.Contains(targetObject.GetType()))
+            {
+                return Array.Empty<ValidationIssue>();
+            }
+            
             context ??= targetObject;
             var result = new List<ValidationIssue>();
             using var serializedObject = new SerializedObject(targetObject);
