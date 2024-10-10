85007c08cab8c2235022f794307ccc14227ace58
Igor Semenov <igor.semenov.minsk@gmail.com>
2024-10-10 14:07:50 +0200

    Additional context menu

--- Assets/EonValidation/Editor/EonValidationContextMenu.cs
+++ Assets/EonValidation/Editor/EonValidationContextMenu.cs
@@ -6,7 +6,7 @@ namespace EonValidation.Editor
     public static class EonValidationContextMenu
     {
         [MenuItem("Assets/EonValidation/Clear Missing References")]
-        public static void ClearMissingReferences()
+        public static void ClearMissingReferencesAssets()
         {
             foreach (var guid in Selection.assetGUIDs)
             {
@@ -21,6 +21,19 @@ namespace EonValidation.Editor
                 if (!any)
                 {
                     Debug.Log($"No missing references in '{assetPath}'");
+                }
+            }
+        }
+
+        [MenuItem("GameObject/EonValidation/Clear Missing References", false, 0)]
+        public static void ClearMissingReferencesGameObjects()
+        {
+            foreach (var gameObject in Selection.gameObjects)
+            {
+                var any = MissingReferenceUtils.ClearMissingReferences(gameObject, true);
+                if (!any)
+                {
+                    Debug.Log($"No missing references in '{gameObject.name}'");
                 }
             }
         }
